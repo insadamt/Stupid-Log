1e5e57cda206b45aae5847089100d7079cf69741
insayd Cyan <94764720+insadamt@users.noreply.github.com>
2026-05-23 23:57:15 +0100

    Remove obsolete stats tab rail overrides

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,28 +7,6 @@
         <title inertia>{{ config('app.name', 'Stupid Log') }}</title>
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
-        <style>
-            .grid-rows-\[120px_minmax\(0\,1fr\)_86px\] {
-                grid-template-rows: 120px minmax(0, 1fr) 96px !important;
-            }
-
-            .h-\[86px\].shrink-0.justify-center {
-                height: 96px !important;
-                padding-top: 14px !important;
-                padding-bottom: 14px !important;
-            }
-
-            .h-\[86px\].shrink-0.justify-center > .overflow-x-auto.rounded-\[26px\].bg-black {
-                overflow-x: hidden !important;
-                overflow-y: hidden !important;
-                padding: 14px 18px !important;
-                scrollbar-width: none !important;
-            }
-
-            .h-\[86px\].shrink-0.justify-center > .overflow-x-auto.rounded-\[26px\].bg-black::-webkit-scrollbar {
-                display: none !important;
-            }
-        </style>
         @inertiaHead
     </head>
     <body>
